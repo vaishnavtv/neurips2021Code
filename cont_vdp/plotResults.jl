3a062a543d2a56c1b386a576f90ae91f65681d73
vaishnavtv <vish0908@gmail.com>
2021-07-21 19:32:16 -0500

    pyplot cont.

--- cont_vdp/plotResults.jl
+++ cont_vdp/plotResults.jl
@@ -1,7 +1,7 @@
 ## Plot the results of the bsaeline-PINNs implementation for the Van der Pol Rayleigh oscillator
-using JLD2, Plots, NeuralPDE, ModelingToolkit, LinearAlgebra, Flux, Trapz, Printf, DiffEqFlux
+using JLD2, PyPlot, NeuralPDE, ModelingToolkit, LinearAlgebra, Flux, Trapz, Printf, DiffEqFlux
 @variables x1,x2
-gr();
+# gr();
 # pygui(true);
 
 
@@ -30,7 +30,7 @@ parameterless_type_θ = DiffEqBase.parameterless_type(optParam);
 chain1 = Chain(Dense(2,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,1));
 chain2 = Chain(Dense(2,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,1));
 chain = [chain1; chain2];
-initθ = map(c -> Float64.(c), DiffEqFlux.initial_params.(chain));
+
 phi = NeuralPDE.get_phi.(chain, parameterless_type_θ);
 
 maxval = 2.0f0;
@@ -48,7 +48,6 @@ rho_true = Float32.([rhoTrue([x,y]) for x in xs, y in ys]);
 rho_pred_norm = rho_pred/trapz((xs,ys),rho_pred);
 rho_true_norm = rho_true/trapz((xs,ys),rho_true);
 
-# gridX, gridY = (VectorizedRoutines.Matlab.meshgrid(xs, ys));
 gridX = xs'.*ones(nEvalFine);
 gridY = ones(nEvalFine)'.*ys;
 
@@ -56,37 +55,34 @@ rho_pred_norm = Float32.(reshape(rho_pred_norm, (length(ys), length(xs))));
 rho_true_norm = Float32.(reshape(rho_true_norm, (length(ys), length(xs))));
 gridX = Float32.(gridX); gridY = Float32.(gridY);
 ##
-using Plots; gr();
-# pygui(true);
-# figure(1, (12,4)); clf();
-# subplot(1,3,1);
-p1 = contourf(xs,ys,rho_pred_norm);#, cmap="inferno", shading = "auto");
-p2 = contourf(xs, ys, rho_true_norm);
-# colorbar();
-# xlabel("x1"); ylabel("x2");
-# title("Prediction");
-# axis("auto");
-# tight_layout();
+# using Plots; gr();
+pygui(true);
+figure(1, (12,4)); clf();
+subplot(1,3,1);
+p1 = pcolor(gridX,gridY,rho_pred_norm, cmap="inferno", shading = "auto");
+colorbar();
+xlabel("x1"); ylabel("x2");
+title("Prediction");
+axis("auto");
+tight_layout();
 
-# subplot(1,3,2);
-# PyPlot.pcolor(gridX, gridY, rho_true_norm, cmap="inferno",shading = "auto");
-# colorbar();xlabel("x1"); ylabel("x2");
-# title("Exact");
-# axis("auto");
-# tight_layout();
+subplot(1,3,2);
+PyPlot.pcolor(gridX, gridY, rho_true_norm, cmap="inferno",shading = "auto");
+colorbar();xlabel("x1"); ylabel("x2");
+title("Exact");
+axis("auto");
+tight_layout();
 
 errNorm = abs.(rho_true_norm - rho_pred_norm);
 mseRHOErr = sum(errNorm[:].^2)/length(errNorm);
 mseRHOErrStr = @sprintf "%.2e" mseRHOErr;
 
-p3 = contourf(xs, ys, errNorm);
-plot(p1, p2, p3, layout = (1,3));
-# subplot(1,3,3);
-# PyPlot.pcolor(gridX, gridY, errNorm, cmap="inferno",shading = "auto");
-# colorbar();xlabel("x1"); ylabel("x2");
-# title(L"Solution Error; $ϵ_{ρ}=$ %$mseRHOErrStr");
-# axis("auto");
-# tight_layout();
+subplot(1,3,3);
+PyPlot.pcolor(gridX, gridY, errNorm, cmap="inferno",shading = "auto");
+colorbar();xlabel("x1"); ylabel("x2");
+title(L"Solution Error; $ϵ_{ρ}=$ %$mseRHOErrStr");
+axis("auto");
+tight_layout();
 
 
 
