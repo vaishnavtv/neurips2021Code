ac5e449374f28e71e91abce055fa0bd2ae09f479
Vaishnav <vish0908@gmail.com>
2021-09-08 17:10:43 -0500

    exp8, tanh, only 20 nn.

--- f16/ll_quasi_f16.jl
+++ f16/ll_quasi_f16.jl
@@ -18,22 +18,22 @@ import Random:seed!;
 seed!(1);
 
 # parameters for neural network
-nn = 100; # number of neurons in the hidden layers
-activFunc = sigmoid; # activation function
+nn = 20; # number of neurons in the hidden layers
+activFunc = tanh; # activation function
 opt1 = ADAM(1e-3); # primary optimizer used for training
 maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 1000; # maximum number of training iterations for opt2
 α_bc = 0.1; # weight on boundary loss
 
-expNum = 7;
+expNum = 8;
 saveFile = "data_ll_quasi/ll_quasi_f16$(expNum).jld2";
 runExp = true; # flag to check if running batch file
 runExp_fileName = ("out_ll_quasi/log$(expNum).txt");
 if runExp
     open(runExp_fileName, "a+") do io
         write(io, "Running ll_quasi_f16_ using QuasiMonteCarlo strategy on CPU. pdelossfunction fixed. BC_losses coefficient: $(α_bc).
-        $(nn) neurons in the 3 hidden layers with $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM $(opt1.eta) and then $(maxOpt2Iters) iterations with LBFGS. Updated quadrature strategy to use UniformSample() alg. \nExperiment number: $(expNum).\n")
+        $(nn) neurons in the 3 hidden layers with $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM $(opt1.eta) and then $(maxOpt2Iters) iterations with LBFGS. Updated quadrature strategy to use UniformSample() alg. Resampling. \nExperiment number: $(expNum).\n")
     end
 end
 
