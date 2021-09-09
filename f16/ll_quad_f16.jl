e1648319436c6037e926606cacb244f8d7ab2e85
Vaishnav <vish0908@gmail.com>
2021-09-09 00:01:06 -0500

    exp17 with 1e-3 lr.

--- f16/ll_quad_f16.jl
+++ f16/ll_quad_f16.jl
@@ -20,13 +20,13 @@ seed!(1);
 # parameters for neural network
 nn = 100; # number of neurons in the hidden layers
 activFunc = relu; # activation function
-opt1 = ADAM(1e-5); # primary optimizer used for training
+opt1 = ADAM(1e-3); # primary optimizer used for training
 maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.BFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 1000; # maximum number of training iterations for opt2
 α_bc = 0.01;
 
-expNum = 16;
+expNum = 17;
 saveFile = "data_ll_quad/ll_quad_f16_$(expNum).jld2";
 runExp = true; # flag to check if running batch file
 runExp_fileName = ("out_ll_quad/log$(expNum).txt");
