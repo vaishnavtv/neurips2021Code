48e9834b19b8271c9abc6e339a4a8d71e45607d5
Vaishnav <vish0908@gmail.com>
2021-09-19 10:00:50 -0500

    quasi_exp11 with g=[0,1]. Q_fpke = 0.1

--- missile/missileDynamics.jl
+++ missile/missileDynamics.jl
@@ -51,7 +51,7 @@ function f(x) # controlled nonlinear dynamics
 end
 
 function g(x)
-    out = Float32.([1.0; 0.0].*1.0I(2)); # diffusion in α
+    out = Float32.([0.0; 1.0].*1.0I(2)); # diffusion in α
     return CUDA.adapt(DiffEqBase.parameterless_type(x),out) 
     # return [0.0; 1.0].*1.0I(2); # diffusion in alpha
     # M = x[1]; a = x[2];
