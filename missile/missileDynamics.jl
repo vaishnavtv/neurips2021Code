afc43f3919ae1a1e7bd267f6ebd1e6101229361b
Vaishnav <vish0908@gmail.com>
2021-09-19 11:11:16 -0500

    quasi_exp18 ForwardTime with g = [1,0]. Q = 0.1.

--- missile/missileDynamics.jl
+++ missile/missileDynamics.jl
@@ -51,7 +51,7 @@ function f(x) # controlled nonlinear dynamics
 end
 
 function g(x)
-    out = Float32.([1.0; 1.0].*1.0I(2)); # diffusion in α
+    out = Float32.([1.0; 0.0].*1.0I(2)); # diffusion in α
     # M = x[1]; a = x[2];
     # out = [-0.0403f0*M^2*sin(a); 0.0f0] 
     return CUDA.adapt(DiffEqBase.parameterless_type(x),out) 
