e237bf83f5692d947b16a19abdf3ecfb424a946d
Vaishnav <vish0908@gmail.com>
2021-09-24 17:07:25 -0500

    ts_vdp grid_exp10 with LBFGS.

--- ts_vdp/ts_ll_grid_vdp.jl
+++ ts_vdp/ts_ll_grid_vdp.jl
@@ -10,19 +10,19 @@ import Random:seed!; seed!(1);
 ## parameters for neural network
 nn = 48; # number of neurons in the hidden layer
 activFunc = tanh; # activation function
-# opt1 = Optim.BFGS(); # primary optimizer used for training
-opt1 = ADAM(1e-3) #Flux.Optimiser(ADAM(1e-3));
+opt1 = Optim.LBFGS(); # primary optimizer used for training
+# opt1 = ADAM(1e-3) #Flux.Optimiser(ADAM(1e-3));
 maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 1000; # maximum number of training iterations for opt2
 
-dx = [0.05f0; 0.05f0; 0.1f0]; # discretization size used for training
+dx = [0.1f0; 0.1f0; 0.1f0]; # discretization size used for training
 tEnd = 10.0f0; 
 Q_fpke = 0.1f0; # Q_fpke = σ^2
 
 # file location to save data
 suff = string(activFunc);
-expNum = 9;
+expNum = 10;
 runExp = true;
 useGPU = true;
 cd(@__DIR__);
@@ -30,8 +30,8 @@ saveFile = "dataTS_grid/ll_ts_vdp_exp$(expNum).jld2";
 runExp_fileName = "out_grid/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Transient vdp with grid training in η. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS.  Q_fpke = $(Q_fpke). Using GPU.
-        Smaller dx = $(dx). tEnd = $(tEnd).
+        write(io, "Transient vdp with grid training in η. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with LBFGS and then $(maxOpt2Iters) with LBFGS.  Q_fpke = $(Q_fpke). Using GPU.
+        dx = $(dx). tEnd = $(tEnd).
         Experiment number: $(expNum)\n")
     end
 end
