1dc839705dcc3aaef6e996e758518da956ce67ef
Vaishnav <vish0908@gmail.com>
2021-09-28 15:08:34 -0500

    ss_vdp grid_exp5 with norm_loss_function.

--- baseline_vdp/ll_grid_vdp.jl
+++ baseline_vdp/ll_grid_vdp.jl
@@ -1,31 +1,34 @@
 ## Solve the FPKE for the Van der Pol oscillator using baseline PINNs (large training set)
-
+cd(@__DIR__);
 using NeuralPDE, Flux, ModelingToolkit, GalacticOptim, Optim, Symbolics, JLD2, DiffEqFlux
 
 using CUDA
 CUDA.allowscalar(false)
 import Random:seed!; seed!(1);
 
+using Quadrature, Cubature
+
 ## parameters for neural network
 nn = 48; # number of neurons in the hidden layer
 activFunc = tanh; # activation function
-opt1 = Optim.BFGS() ; # primary optimizer used for training
-maxOpt1Iters = 50000; # maximum number of training iterations for opt1
+opt1 = Optim.BFGS(); # primary optimizer used for training
+maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 1000; # maximum number of training iterations for opt2
-
 
 dx = 0.05; # discretization size used for training
 
 # file location to save data
 suff = string(activFunc);
-expNum = 4;
+expNum = 5;
 saveFile = "data_grid/ll_grid_vdp_exp$(expNum).jld2";
+useGPU = false;
 runExp = true;
 runExp_fileName = "out_grid/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Steady State vdp with Grid training. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with BFGS and then $(maxOpt2Iters) with LBFGS. Not Using GPU. 
+        write(io, "Steady State vdp with Grid training. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with BFGS and then $(maxOpt2Iters) with LBFGS. Not using GPU. 
+        Adding norm loss function.
         Experiment number: $(expNum)\n")
     end
 end
@@ -43,7 +46,7 @@ function g(x::Vector)
 end
 
 # PDE
-Q = 0.1f0; # Q = σ^2
+Q_fpke = 0.1f0; # Q = σ^2
 ρ(x) = exp(η(x[1],x[2]));
 F = f(x)*ρ(x);
 G = 0.5f0*(g(x)*Q*g(x)')*ρ(x);
@@ -53,7 +56,19 @@ T2 = sum([(Differential(x[i])*Differential(x[j]))(G[i,j]) for i in 1:length(x), 
 
 Eqn = expand_derivatives(-T1+T2); # + dx*u(x1,x2)-1 ~ 0;
 pdeOrig = simplify(Eqn/ρ(x)) ~ 0.0f0;
-pde = (0.05f0Differential(x2)(Differential(x2)(η(x1, x2)))*exp(η(x1, x2)) + 0.05f0exp(η(x1, x2))*(Differential(x2)(η(x1, x2))^2) - (exp(η(x1, x2))*(1 - (x1^2))) - (x2*Differential(x1)(η(x1, x2))*exp(η(x1, x2))) - (Differential(x2)(η(x1, x2))*exp(η(x1, x2))*(x2*(1 - (x1^2)) - x1)))*(exp(η(x1, x2))^-1) ~ 0.0f0  # simplified pde rewritten with constants in float32 format
+# pde = pdeOrig;
+# pde = (0.05f0Differential(x2)(Differential(x2)(η(x1, x2)))*exp(η(x1, x2)) + 0.05f0exp(η(x1, x2))*(Differential(x2)(η(x1, x2))^2) - (exp(η(x1, x2))*(1 - (x1^2))) - (x2*Differential(x1)(η(x1, x2))*exp(η(x1, x2))) - (Differential(x2)(η(x1, x2))*exp(η(x1, x2))*(x2*(1 - (x1^2)) - x1)))*(exp(η(x1, x2))^-1) ~ 0.0f0  # simplified pde rewritten with constants in float32 format
+
+# pde = (Differential(x1)(x2*exp(η(x1, x2))) + Differential(x2)(exp(η(x1, x2))*(x2*(1 - (x1^2)) - x1)))*(exp(η(x1, x2))^-1) ~ 0.0f0 # drift term (works, no NaN)
+# pde = Differential(x2)(Differential(x2)((η(x1, x2)))) ~ 0.0f0  # diffusion term 1 (works, no NaN)
+# pde = ((Differential(x2)(η(x1,x2,t)))*(Differential(x2)(η(x1,x2,t)))) ~ 0.0f0 # square of derivative doesn't work
+
+driftTerm = (Differential(x1)(x2*exp(η(x1, x2))) + Differential(x2)(exp(η(x1, x2))*(x2*(1 - (x1^2)) - x1)))*(exp(η(x1, x2))^-1)
+diffTerm1 = Differential(x2)(Differential(x2)(η(x1,x2))) 
+diffTerm2 = abs2(Differential(x2)(η(x1,x2))) # works
+diffTerm = Q_fpke/2*(diffTerm1 + diffTerm2); # diffusion term
+
+pde = driftTerm - diffTerm ~ 0.0f0 # full pde
 
 ## Domain
 maxval = 4.0f0;
@@ -68,7 +83,10 @@ bcs = [ρ([-maxval,x2]) ~ 0.f0, ρ([maxval,x2]) ~ 0,
 dim = 2 # number of dimensions
 chain = Chain(Dense(dim,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,1));
 
-initθ = DiffEqFlux.initial_params(chain) #|> gpu;
+initθ = DiffEqFlux.initial_params(chain) 
+if useGPU
+    initθ = initθ |> gpu;
+end
 flat_initθ = initθ
 eltypeθ = eltype(flat_initθ);
 parameterless_type_θ = DiffEqBase.parameterless_type(flat_initθ);
@@ -114,9 +132,11 @@ _bc_loss_functions = [
 
 train_domain_set, train_bound_set =
     NeuralPDE.generate_training_sets(domains, dx, [pde], bcs, eltypeθ, indvars, depvars) ;
-train_domain_set = train_domain_set #|> gpu;
-train_bound_set = train_bound_set #|> gpu;
-
+if useGPU
+    train_domain_set = train_domain_set |> gpu;
+    train_bound_set = train_bound_set |> gpu;
+end
+    
 pde_loss_function = NeuralPDE.get_loss_function(
     _pde_loss_function,
     train_domain_set[1],
@@ -131,12 +151,23 @@ bc_loss_functions = [
     (loss, set) in zip(_bc_loss_functions, train_bound_set)
 ]
 
-
 bc_loss_function_sum = θ -> sum(map(l -> l(θ), bc_loss_functions))
 @show bc_loss_function_sum(initθ)
 
+## additional loss function
+lbs = [-maxval, -maxval]; ubs = [maxval, maxval];
+function norm_loss_function(θ)
+    function inner_f(x,θ)
+         return exp(sum(phi(x, θ))) # density
+    end
+    prob = QuadratureProblem(inner_f, lbs, ubs, θ)
+    norm2 = solve(prob, HCubatureJL(), reltol = 1e-8, abstol = 1e-8, maxiters =10);
+    return abs2(norm2[1] - 1)
+end
+@show norm_loss_function(initθ)
+
 function loss_function_(θ, p)
-    return pde_loss_function(θ) + bc_loss_function_sum(θ) 
+    return pde_loss_function(θ) + bc_loss_function_sum(θ) + norm_loss_function(θ)
 end
 @show loss_function_(initθ,0)
 
@@ -147,7 +178,12 @@ prob = GalacticOptim.OptimizationProblem(f_, initθ)
 nSteps = 0;
 PDE_losses = Float32[];
 BC_losses = Float32[];
+NORM_losses = Float32[];
 cb_ = function (p, l)
+    if any(isnan.(p))
+        println("SOME PARAMETERS ARE NaN.")
+    end
+
     global nSteps = nSteps + 1
     println("[$nSteps] Current loss is: $l")
     println(
@@ -159,13 +195,14 @@ cb_ = function (p, l)
 
     push!(PDE_losses, pde_loss_function(p))
     push!(BC_losses, bc_loss_function_sum(p))
+    push!(NORM_losses, norm_loss_function(p))
 
     if runExp # if running job file
         open(runExp_fileName, "a+") do io
             write(io, "[$nSteps] Current loss is: $l \n")
         end;
         
-        jldsave(saveFile; optParam=Array(p), PDE_losses, BC_losses);
+        jldsave(saveFile; optParam=Array(p), PDE_losses, BC_losses, NORM_losses );
     end
     return false
 end
@@ -179,5 +216,5 @@ println("Optimization done.");
 ## Save data
 cd(@__DIR__);
 if runExp
-    jldsave(saveFile;optParam = Array(res.minimizer), PDE_losses, BC_losses);
+    jldsave(saveFile;optParam = Array(res.minimizer), PDE_losses, BC_losses, NORM_losses);
 end
