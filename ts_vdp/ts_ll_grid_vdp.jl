9cf81ebfcd018f70019c890a9a4b7a280ae4788b
Vaishnav <vish0908@gmail.com>
2021-09-29 16:32:46 -0500

    ts_vdp grid_exp28 with SE and 1000 alpha_ic loss for IC.

--- ts_vdp/ts_ll_grid_vdp.jl
+++ ts_vdp/ts_ll_grid_vdp.jl
@@ -19,11 +19,11 @@ maxOpt2Iters = 1000; # maximum number of training iterations for opt2
 dx = [0.1f0; 0.1f0; 0.1f0]; # discretization size used for training
 tEnd = 10.0f0; 
 Q_fpke = 0.1f0; # Q_fpke = σ^2
-α_ic = 0.0; # weight on initial loss
+α_ic = 1000.0; # weight on initial loss
 
 # file location to save data
 suff = string(activFunc);
-expNum = 26;
+expNum = 28;
 runExp = true;
 useGPU = true;
 cd(@__DIR__);
@@ -33,7 +33,7 @@ if runExp
     open(runExp_fileName, "a+") do io
         write(io, "Transient vdp with grid training in η. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with LBFGS and then $(maxOpt2Iters) with LBFGS.  Q_fpke = $(Q_fpke). Using GPU.
         dx = $(dx). tEnd = $(tEnd). Enforcing steady-state. Enforcing BC. Fixed drift term. 
-        α_ic = $(α_ic). No IC. IC_losses will return SS condition losses.
+        α_ic = $(α_ic). IC as before (uniform). Using SE instead of MSE. Dramatically large α_ic.
         Experiment number: $(expNum)\n")
     end
 end
@@ -91,12 +91,12 @@ domains = [x1 ∈ IntervalDomain(-maxval,maxval),
            t ∈ IntervalDomain(0.0f0, tEnd)];
 
 ssExp  =  Dt((η(x1,x2,tEnd)));
-ρ_ic(x) = exp(η(x[1],x[2],0.0)); 
+ρ_ic(x) = exp(η(x[1],x[2],0.0f0)); 
 icExp = ρ_ic(xSym)
 # Initial and Boundary conditions
 bcs = [ρ([-maxval,x2]) ~ 0.0f0, ρ([maxval,x2]) ~ 0.0f0,
        ρ([x1,-maxval]) ~ 0.0f0, ρ([x1,maxval]) ~ 0.0f0, 
-    #    icExp ~ rho0([x1,x2]), #0.00015625f0, # initial condition
+       icExp ~ 0.00015625f0, # initial condition
        ssExp ~ 0.0f0]; # steady-state condition
 
 ## Neural network
@@ -172,7 +172,7 @@ bc_loss_functions = [
     (loss, set) in zip(_bc_loss_functions, train_bound_set)
 ]
 
-ic_loss_function = (θ) -> mean(abs2,_bc_loss_functions[5](train_bound_set[5], θ));
+ic_loss_function = (θ) -> sum(abs2,_bc_loss_functions[5](train_bound_set[5], θ));
 @show ic_loss_function(initθ)
 
 bc_loss_function_sum = θ -> sum(map(l -> l(θ), bc_loss_functions))
