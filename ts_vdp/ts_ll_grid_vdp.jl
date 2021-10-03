ebd35622ade6ad4c65ee027e75a3626f914647d5
Vaishnav <vish0908@gmail.com>
2021-10-03 11:40:56 -0500

    ts_vdp grid_exp36 with very small tEnd, very small Q+fpke.

--- ts_vdp/ts_ll_grid_vdp.jl
+++ ts_vdp/ts_ll_grid_vdp.jl
@@ -16,14 +16,14 @@ maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 1000; # maximum number of training iterations for opt2
 
-dx = [0.1f0; 0.1f0; 0.01f0]; # discretization size used for training
-tEnd = 1.0f0; 
+dx = [0.1f0; 0.1f0; 0.001f0]; # discretization size used for training
+tEnd = 0.1f0; 
 Q_fpke = 0.001f0; # Q_fpke = σ^2
 α_ic = 0.0; # weight on initial loss
 
 # file location to save data
 suff = string(activFunc);
-expNum = 35;
+expNum = 36;
 runExp = true;
 useGPU = true;
 cd(@__DIR__);
@@ -33,7 +33,7 @@ if runExp
     open(runExp_fileName, "a+") do io
         write(io, "Transient vdp with grid training in η. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with LBFGS and then $(maxOpt2Iters) with LBFGS.  Q_fpke = $(Q_fpke). Using GPU.
         dx = $(dx). tEnd = $(tEnd). Not enforcing steady-state. Enforcing BC. Fixed drift term. 
-        α_ic = $(α_ic). No extra weight on IC. Using Gaussian at t0. Very small Q_fpke.
+        α_ic = $(α_ic). No extra weight on IC. Using Gaussian at t0. Very small Q_fpke. Very small tEnd.
         Experiment number: $(expNum)\n")
     end
 end
