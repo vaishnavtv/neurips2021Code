3cfd5b23d70c0172b54d9fd329174280e25c4c8e
Vaishnav <vish0908@gmail.com>
2021-10-05 12:43:48 -0500

    ts_linsys conitnuity.

--- ts_linsys/ts_ll_grid_ls.jl
+++ ts_linsys/ts_ll_grid_ls.jl
@@ -10,29 +10,28 @@ import Random:seed!; seed!(1);
 ## parameters for neural network
 nn = 48; # number of neurons in the hidden layer
 activFunc = tanh; # activation function
-# opt1 = Optim.LBFGS(); # primary optimizer used for training
-opt1 = ADAM(1e-3) #Flux.Optimiser(ADAM(1e-3));
+opt1 = Optim.LBFGS(); # primary optimizer used for training
+# opt1 = ADAM(1e-3) #Flux.Optimiser(ADAM(1e-3));
 maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 1000; # maximum number of training iterations for opt2
 
 dx = [0.1f0; 0.1f0; 0.01f0]; # discretization size used for training
 tEnd = 1.0f0; 
-Q_fpke = 0.001f0; # Q_fpke = σ^2
+Q_fpke = 0.00f0; # Q_fpke = σ^2
 α_ic = 0.0f0; # weight on initial loss
 
 # file location to save data
 suff = string(activFunc);
-expNum = 2;
-runExp = true;
+expNum = 3;
+runExp = false;
 useGPU = true;
 cd(@__DIR__);
 saveFile = "dataTS_grid/ll_ts_ls_exp$(expNum).jld2";
 runExp_fileName = "out_grid/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Transient linear system with grid training in η. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS.  Q_fpke = $(Q_fpke). Using GPU.
-        dx = $(dx). tEnd = $(tEnd). Not enforcing steady-state. Enforcing BC. Diffusion only in x2.
+        write(io, "Transient linear system with grid training in η. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with LBFGS and then $(maxOpt2Iters) with LBFGS.  Q_fpke = $(Q_fpke). Using GPU. dx = $(dx). tEnd = $(tEnd). Not enforcing steady-state. Enforcing BC. No diffusion. Solving continuity.
         α_ic = $(α_ic). No IC. 
         Experiment number: $(expNum)\n")
     end
@@ -55,29 +54,47 @@ end
 Dt = Differential(t); 
 Dx2 = Differential(x2);
 ρ(x) = exp(η(x[1],x[2],t)); # time-varying density
-F = f(xSym)*ρ(xSym);
-G = 0.5f0*(g(xSym)*Q_fpke*g(xSym)')*ρ(xSym);
+# F = f(xSym)*ρ(xSym);
+# G = 0.5f0*(g(xSym)*Q_fpke*g(xSym)')*ρ(xSym);
 
-T1 = sum([Differential(xSym[i])(F[i]) for i in 1:length(xSym)]);
-T2 = sum([(Differential(xSym[i])*Differential(xSym[j]))(G[i,j]) for i in 1:length(xSym), j=1:length(xSym)]);
+# T1 = sum([Differential(xSym[i])(F[i]) for i in 1:length(xSym)]);
+# T2 = sum([(Differential(xSym[i])*Differential(xSym[j]))(G[i,j]) for i in 1:length(xSym), j=1:length(xSym)]);
 # T1 = sum([Symbolics.derivative(F[i], xSym[i]) for i = 1:length(xSym)]); # pde drift term
 # T2 = sum([
 #     Symbolics.derivative(Symbolics.derivative(G[i, j], xSym[i]), xSym[j]) for i = 1:length(xSym), j = 1:length(xSym)
 # ]); # pde diffusion term
 
-pdeOrig = simplify((Dt(ρ(xSym)) + T1 - T2)/ρ(xSym), expand=true) ~ 0.0f0;
-pde = pdeOrig;
+# pdeOrig = simplify((Dt(ρ(xSym)) + T1 - T2)/ρ(xSym), expand=true) ~ 0.0f0;
+# pde = pdeOrig;
 
 # pde = (Differential(t)(exp(η(x1, x2, t))) + Differential(x1)(x2*exp(η(x1, x2, t))) + Differential(x2)(exp(η(x1, x2, t))*(x2*(1 - (x1^2)) - x1)))*(exp(η(x1, x2, t))^-1) ~ 0.0f0 # drift term works
 # diffTerm = 0.5f0*Q_fpke^2*(Differential(x2)(Differential(x2)(η(x1,x2,t))))# diffusion hessian term works
 # diffTerm = ((Differential(x2)(η(x1,x2,t)))*(Differential(x2)(η(x1,x2,t)))) # square of derivative doesn't work
 
-driftTerm = T1*(exp(η(x1, x2, t))^-1)
-diffTerm1 = Differential(x2)(Differential(x2)(η(x1,x2,t))) 
-diffTerm2 = abs2(Differential(x2)(η(x1,x2,t))) # works
-diffTerm = Q_fpke/2*(diffTerm1 + diffTerm2); # diffusion term
+# # # Breaking PDE down (correct for when diffusion only in x2)
+# driftTerm = T1*(exp(η(x1, x2, t))^-1)
+# diffTerm1 = Differential(x2)(Differential(x2)(η(x1,x2,t))) 
+# diffTerm2 = abs2(Differential(x2)(η(x1,x2,t))) # works
+# diffTerm = Q_fpke/2*(diffTerm1 + diffTerm2); # diffusion term
 
 # pde = Dt(η(x1,x2,t)) + driftTerm - diffTerm ~ 0.0f0 # full pde
+
+##
+## Writing PDE in terms of η directly - convoluted
+diffC = 0.5f0*(g(xSym)*Q_fpke*g(xSym)'); # diffusion term
+ηx = η(xSym[1],xSym[2],t);
+G2 = diffC*ηx;
+
+T1 = sum([(Differential(xSym[i])(f(xSym)[i]) + (f(xSym)[i]* Differential(xSym[i])(ηx))) for i in 1:length(xSym)]); # drift term
+T2 = sum([(Differential(xSym[i])*Differential(xSym[j]))(G2[i,j]) for i in 1:length(xSym), j=1:length(xSym)]);
+T2 += sum([(Differential(xSym[i])*Differential(xSym[j]))(diffC[i,j]) - (Differential(xSym[i])*Differential(xSym[j]))(diffC[i,j])*ηx + diffC[i,j]*(Differential(xSym[i])(ηx))*(Differential(xSym[j])(ηx)) for i in 1:length(xSym), j=1:length(xSym)]); # complete diffusion term
+
+
+
+Eqn = Dt(ηx) + expand_derivatives(-T1+T2); 
+pdeOrig2 = simplify(Eqn, expand = true) ~ 0.0f0;
+
+pde = pdeOrig2;
 
 ## Domain
 maxval = 4.0; 
