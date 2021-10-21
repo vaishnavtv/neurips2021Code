82155e91eaf63b8a454e1aea18d0cc1757f5d803
Vaishnav <vish0908@gmail.com>
2021-10-21 12:15:09 -0500

    ss_vdp quasi_exp5

--- baseline_vdp/ll_quasi_vdp.jl
+++ baseline_vdp/ll_quasi_vdp.jl
@@ -2,8 +2,8 @@
 
 using NeuralPDE, Flux, ModelingToolkit, GalacticOptim, Optim, Symbolics, JLD2, DiffEqFlux
 
-using CUDA
-CUDA.allowscalar(false)
+# using CUDA
+# CUDA.allowscalar(false)
 
 import Random:seed!; seed!(1);
 using QuasiMonteCarlo
@@ -11,20 +11,20 @@ using QuasiMonteCarlo
 ## parameters for neural network
 nn = 48; # number of neurons in the hidden layer
 activFunc = tanh; # activation function
-opt1 = Optim.BFGS(); # primary optimizer used for training
+opt1 = ADAM(1e-3); # primary optimizer used for training
 maxOpt1Iters = 10000; # maximum number of training iterations for opt1
-opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
-maxOpt2Iters = 1000; # maximum number of training iterations for opt2
+opt2 = Optim.BFGS(); # second optimizer used for fine-tuning
+maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 
 # file location to save data
 suff = string(activFunc);
-expNum = 4;
+expNum = 5;
 saveFile = "data_quasi/ll_quasi_vdp_exp$(expNum).jld2";
 runExp = true;
 runExp_fileName = "out_quasi/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Steady State vdp with QuasiMonteCarlo training. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with LBFGS and then $(maxOpt2Iters) with LBFGS. Not using GPU. 
+        write(io, "Steady State vdp with QuasiMonteCarlo training. 2 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. Not using GPU. UniformSample strategy. PDE written directly in η.
         Experiment number: $(expNum)\n")
     end
 end
@@ -32,7 +32,7 @@ end
 @parameters x1, x2
 @variables  η(..)
 
-x = [x1;x2]
+xSym = [x1;x2]
 
 # Van der Pol Dynamics
 f(x) = [x[2]; -x[1] + (1-x[1]^2)*x[2]];
@@ -42,18 +42,31 @@ function g(x::Vector)
 end
 
 # PDE
-Q = 0.1f0; # Q = σ^2
+Q_fpke = 0.1f0; # Q = σ^2
 ρ(x) = exp(η(x[1],x[2]));
-F = f(x)*ρ(x);
-G = 0.5f0*(g(x)*Q*g(x)')*ρ(x);
+F = f(xSym)*ρ(xSym);
+G = 0.5f0*(g(xSym)*Q_fpke*g(xSym)')*ρ(xSym);
 
-T1 = sum([Differential(x[i])(F[i]) for i in 1:length(x)]);
-T2 = sum([(Differential(x[i])*Differential(x[j]))(G[i,j]) for i in 1:length(x), j=1:length(x)]);
+T1 = sum([Differential(xSym[i])(F[i]) for i in 1:length(xSym)]);
+T2 = sum([(Differential(xSym[i])*Differential(xSym[j]))(G[i,j]) for i in 1:length(xSym), j=1:length(xSym)]);
 
 Eqn = expand_derivatives(-T1+T2); # + dx*u(x1,x2)-1 ~ 0;
-pdeOrig = simplify(Eqn/ρ(x)) ~ 0.0f0;
+pdeOrig = simplify(Eqn/ρ(xSym)) ~ 0.0f0;
 pde = pdeOrig;
 # pde = (0.05f0Differential(x2)(Differential(x2)(η(x1, x2)))*exp(η(x1, x2)) + 0.05f0exp(η(x1, x2))*(Differential(x2)(η(x1, x2))^2) - (exp(η(x1, x2))*(1 - (x1^2))) - (x2*Differential(x1)(η(x1, x2))*exp(η(x1, x2))) - (Differential(x2)(η(x1, x2))*exp(η(x1, x2))*(x2*(1 - (x1^2)) - x1)))*(exp(η(x1, x2))^-1) ~ 0.0f0  # simplified pde rewritten with constants in float32 format
+
+# Equation written directly in η
+diffC = 0.5f0*(g(xSym)*Q_fpke*g(xSym)'); # diffusion term
+G2 = diffC*η(xSym...);
+
+T1_2 = sum([(Differential(xSym[i])(f(xSym)[i]) + (f(xSym)[i]* Differential(xSym[i])(η(xSym...)))) for i in 1:length(xSym)]); # drift term
+T2_2 = sum([(Differential(xSym[i])*Differential(xSym[j]))(G2[i,j]) for i in 1:length(xSym), j=1:length(xSym)]);
+T2_2 += sum([(Differential(xSym[i])*Differential(xSym[j]))(diffC[i,j]) - (Differential(xSym[i])*Differential(xSym[j]))(diffC[i,j])*η(xSym...) + diffC[i,j]*(Differential(xSym[i])(η(xSym...)))*(Differential(xSym[j])(η(xSym...))) for i in 1:length(xSym), j=1:length(xSym)]); # complete diffusion term
+
+Eqn = expand_derivatives(-T1_2+T2_2); 
+pdeOrig2 = simplify(Eqn, expand = true) ~ 0.0f0;
+
+pde = pdeOrig2;
 
 ## Domain
 maxval = 4.0;
@@ -73,7 +86,7 @@ flat_initθ = initθ
 eltypeθ = eltype(flat_initθ);
 parameterless_type_θ = DiffEqBase.parameterless_type(flat_initθ);
 
-strategy = NeuralPDE.QuasiRandomTraining(100;sampling_alg=LatinHypercubeSample(), resampling=false,minibatch=1000)
+strategy = NeuralPDE.QuasiRandomTraining(100;sampling_alg=UniformSample(), resampling=false,minibatch=1000)
 
 phi = NeuralPDE.get_phi(chain, parameterless_type_θ);
 derivative = NeuralPDE.get_numeric_derivative();
