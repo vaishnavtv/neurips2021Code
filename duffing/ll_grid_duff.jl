4b89207700490bc86af394d6cf240f63e9b26ed4
Vaishnav <vish0908@gmail.com>
2021-12-18 07:38:57 -0600

    ss_duff grid_exp7 with alphas as in 19-Xu.

--- duffing/ll_grid_duff.jl
+++ duffing/ll_grid_duff.jl
@@ -17,19 +17,19 @@ opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 
 dx = 0.01; # discretization size used for training
-α_pde = 1f0;
-α_bc = 1f0;
+α_pde = 400f0;
+α_bc = 0.1f0;
 
 # file location to save data
 suff = string(activFunc);
-expNum = 6;
+expNum = 7;
 saveFile = "data_grid/ll_grid_duff_exp$(expNum).jld2";
 useGPU = true;
 runExp = true;
 runExp_fileName = "out_grid/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Steady State Duffing oscillator with Grid training. 4 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with $(opt1) and then $(maxOpt2Iters) with $(opt2). Using GPU? = $(useGPU). dx = $(dx).α_pde = $(α_pde). α_bc = $(α_bc). 
+        write(io, "Steady State Duffing oscillator with Grid training. 4 HL with $(nn) neurons in the hl and $(suff) activation. $(maxOpt1Iters) iterations with $(opt1) and then $(maxOpt2Iters) with $(opt2). Using GPU? = $(useGPU). dx = $(dx).α_pde = $(α_pde). α_bc = $(α_bc). Using α loss coefficients as used in 19-Xu_SteadyFP paper.
         Experiment number: $(expNum)\n")
     end
 end
