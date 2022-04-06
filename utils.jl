f3b842eae01f95da6ac4a5b218691adfda480fa1
Vaishnav <vish0908@gmail.com>
2022-04-06 16:30:24 -0500

    ig.

--- utils.jl
+++ utils.jl
@@ -1,5 +1,6 @@
 # Get initial set of params
-using Distributions, LinearAlgebra, Flux
+using Distributions, LinearAlgebra, Flux, CUDA
+import Random:seed!; seed!(1);
 
 function sub2ind(i, N)
     d = length(N)
@@ -43,31 +44,46 @@ end
 # maxval = 4.0;
 # xmin = maxval * [-1, -1];
 # xmax = maxval * [1, 1];
-# nGrid = 100;
+# nGrid = 50;
 # C = GenerateNDGrid(xmin, xmax, nGrid*[1,1]);
 
-# # Initial condition
-# μ0  = [0,0]; Σ0 = 0.1*1.0I(2); #gaussian 
+# # # Initial condition
+# # μ0  = [0,0]; Σ0 = 0.1*1.0I(2); #gaussian 
 # r0 = pdf(MvNormal(μ0, Σ0),C);
 # rho0 = reshape(r0, (1, nGrid^2));
 
 # dim = 2 # number of dimensions
-# nn = 10;
-# chain = Chain(Dense(dim,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,1));
+# nn = 100; activFunc = tanh;
+# chain = Chain(Dense(dim,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,nn,activFunc), Dense(nn,1)) #|> gpu;
 
-# loss(x,y) = Flux.Losses.mse(chain(x),y);
+# loss(x,y) = Flux.Losses.mse(exp.(chain(x)),y);
 # ps = Flux.params(chain);
 # opt = ADAM(1e-3);
-# train_loader = Flux.DataLoader((C,rho0), batchsize=10, shuffle=true);
+# train_loader = Flux.DataLoader(((C),(rho0)))#, batchsize=1, shuffle=true);
 # maxIters = 5000;
+
+# using BenchmarkTools
+# @btime Flux.Optimise.train!(loss, ps, train_loader, opt) ;
+# 8.669 s (24310716 allocations: 1.63 GiB) for batchsize = 1 on gpu
+# 2.036 s (5930000 allocations: 3.51 GiB)  for batchsize = 1 on cpu
+# 9.418 s (24323430 allocations: 1.63 GiB) for full batch on gpu
+# 1.948 s (5930000 allocations: 3.51 GiB) for full batch on cpu 
 
 # Flux.@epochs maxIters Flux.Optimise.train!(loss, ps, train_loader, ADAM(1e-3));
 
 
-# ##
+##
 # XX = reshape(C[1, :], nGrid, nGrid);
 # YY = reshape(C[2, :], nGrid, nGrid);
 # using PyPlot; pygui(true);
+# using Plots;
+# p1 = heatmap(C[1,1:nGrid], C[1,1:nGrid], reshape(r0, (nGrid, nGrid)));
+# # p2 = heatmap(C[1,1:nGrid], C[1,1:nGrid], reshape(Array(exp.(chain((C)))), (nGrid, nGrid)));
+# p2 = heatmap(C[1,1:nGrid], C[1,1:nGrid], reshape(Array(exp.(phi((C),th0))), (nGrid, nGrid)));
+# p3 = heatmap(C[1,1:nGrid], C[1,1:nGrid], reshape(r0, (nGrid, nGrid)) - reshape(Array(exp.(phi((C),th0))), (nGrid, nGrid)));
+# p = plot(p1, p2, p3, aspect_ratio=:equal);
+# display(p);
+##
 # figure(56, (12,4)); clf();
 # subplot(1,3,1);
 # pcolor(XX, YY, reshape(r0, (nGrid, nGrid)), shading = "auto", cmap = "inferno"); colorbar();
