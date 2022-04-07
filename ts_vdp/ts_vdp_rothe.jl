4a36d888b1e530ce2c53aed207ac7fb74e8aa574
Vaishnav <vish0908@gmail.com>
2022-04-06 22:34:15 -0500

    ts_rothe exp5 - dx 0.1, 100 LBFGS iters

--- ts_vdp/ts_vdp_rothe.jl
+++ ts_vdp/ts_vdp_rothe.jl
@@ -15,14 +15,14 @@ maxOpt1Iters = 1000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 100; # maximum number of training iterations for opt2
 
-dx = 0.05; # discretization size used for training
+dx = 0.1; # discretization size used for training
 α_bc = 1.0f0 # weight on boundary conditions loss
 Q_fpke = 0.1f0; # Q = σ^2
-dt = 0.01; tEnd = 5.0;
+dt = 0.01; tEnd = 10.0;
 
 # file location to save data
 suff = string(activFunc);
-expNum = 4;
+expNum = 5;
 saveFile = "data_rothe/vdp_exp$(expNum).jld2";
 useGPU = true; if useGPU using CUDA end;
 runExp = true;
