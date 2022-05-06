ebfc47d526803aa26e718770a09eb25ad79523e8
Vaishnav <vish0908@gmail.com>
2022-05-06 14:44:11 -0500

    ts_cont_vdp exp19: with ac = 1e-4

--- ts_cont_vdp/ts_cont_vdp_rothe.jl
+++ ts_cont_vdp/ts_cont_vdp_rothe.jl
@@ -13,18 +13,18 @@ activFunc = tanh; # activation function
 opt1 = ADAM(1e-3); # primary optimizer used for training
 maxOpt1Iters = 1000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
-maxOpt2Iters = 200; # maximum number of training iterations for opt2
+maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 
 dx = 0.1; # discretization size used for training
-Q_fpke = 0.1f0; # Q = σ^2
+Q_fpke = 0.0f0; # Q = σ^2
 dt = 0.2f0; tEnd = 1.0f0;
 μ0  = [0f0,0f0]; Σ0 = 1f0*1.0f0I(2); #gaussian 
 A = -0.5f0*1.0f0I(2); # stable linear system
-α_c = 0f0; # weight on control effort loss
+α_c = 1f-4; # weight on control effort loss
 
 # file location to save data
 suff = string(activFunc);
-expNum = 14;
+expNum = 19;
 saveFile = "data_cont_rothe/vdp_exp$(expNum).jld2";
 useGPU = true;
 runExp = true;
