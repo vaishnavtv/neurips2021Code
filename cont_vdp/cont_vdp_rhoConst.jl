069aeb7812d5e9bc6e47b779ac690f15a559a304
Vaishnav <vish0908@gmail.com>
2022-05-11 09:50:15 -0500

    cont_rhoFixed exp12: Sigma = 1, Q = 0.1, ~/rho

--- cont_vdp/cont_vdp_rhoConst.jl
+++ cont_vdp/cont_vdp_rhoConst.jl
@@ -22,10 +22,10 @@ maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 Σ_ss = 1f0*1.0f0I(2);
 
 dx = 0.05f0; # discretization size used for training
-Q_fpke = 0.0f0; # Q = σ^2
+Q_fpke = 0.1f0; # Q = σ^2
 
 # file location to save data
-expNum = 11;
+expNum = 12;
 useGPU = true;
 runExp = true;
 saveFile = "data_rhoConst/exp$(expNum).jld2";
