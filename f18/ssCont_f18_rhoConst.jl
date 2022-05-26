613c41e7bd17740ae20165f9abda584b2a4b6814
Vaishnav <vish0908@gmail.com>
2022-05-26 15:44:04 -0500

    exp7: indX, sigma = 0.01

--- f18/ssCont_f18_rhoConst.jl
+++ f18/ssCont_f18_rhoConst.jl
@@ -22,7 +22,7 @@ maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 
 # parameters for rhoSS_desired
 μ_ss = [0f0,0f0,0f0,0f0]# .+ Array(f18_xTrim[indX])
-Σ_ss = 0.1f0*Array(f18_xTrim[indX]).*1.0f0I(4)
+Σ_ss = 0.01f0*Array(f18_xTrim[indX]).*1.0f0I(4)
 maxMult = 1f0; # multiplier for maximum (upper bound)
 
 Q_fpke = 0.0f0; # Q = σ^2
@@ -32,7 +32,7 @@ nMB = 500; # number of minibatches
 
 
 # file location to save data
-expNum = 7;
+expNum = 8;
 useGPU = false;
 runExp = true;
 saveFile = "data_rhoConst/exp$(expNum).jld2";
