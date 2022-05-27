f83f1aba05e4dfbdf601c584308395a4609bf97c
Vaishnav <vish0908@gmail.com>
2022-05-27 10:56:44 -0500

    exp10: using utrim for d_stab (u[3]), but maxMult = 9

--- f18/ssCont_f18_rhoConst.jl
+++ f18/ssCont_f18_rhoConst.jl
@@ -23,7 +23,7 @@ maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 # parameters for rhoSS_desired
 μ_ss = [0f0,0f0,0f0,0f0]# .+ Array(f18_xTrim[indX])
 Σ_ss = 0.01f0*Array(f18_xTrim[indX]).*1.0f0I(4)
-maxMult = 1f0; # multiplier for maximum (upper bound)
+maxMult = 9f0; # multiplier for maximum (upper bound)
 
 Q_fpke = 0.0f0; # Q = σ^2
 
@@ -32,7 +32,7 @@ nMB = 500; # number of minibatches
 
 
 # file location to save data
-expNum = 9;
+expNum = 10;
 useGPU = false;
 runExp = true;
 saveFile = "data_rhoConst/exp$(expNum).jld2";
@@ -40,7 +40,7 @@ runExp_fileName = "out_rhoConst/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
         write(io, "Generating a controller for f18 with desired ss distribution. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Not dividing equation by ρ. Using Quasi sampling strategy for training. nPtsPerMB = $(nPtsPerMB). nMB = $(nMB).
-        Final Distribution Gaussian about trim point. maxMult = $(maxMult). uTrim present, but not in δ_stab(u[3]).
+        Final Distribution Gaussian about trim point. maxMult = $(maxMult). uTrim present, but maxMult = $(maxMult).
         Experiment number: $(expNum)\n")
     end
 end
@@ -82,8 +82,8 @@ function f(xd)
     
     # perturbation about trim point
     xFull = f18_xTrim + maskIndx*xd; 
-    uFull = [1f0;1f0;0f0;1f0].*f18_uTrim + maskIndu*ud;
-    # uFull = f18_uTrim + maskIndu*ud;
+    # uFull = [1f0;1f0;0f0;1f0].*f18_uTrim + maskIndu*ud;
+    uFull = f18_uTrim + maskIndu*ud;
 
     xdotFull = f18Dyn(xFull, uFull)
     # xdotFull = xFull;
