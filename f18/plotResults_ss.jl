df6ea877c25450d29661b6f062f175a8cbb0e3ed
Vaishnav <vish0908@gmail.com>
2022-06-11 18:01:05 -0500

    plot files upd

--- f18/plotResults_ss.jl
+++ f18/plotResults_ss.jl
@@ -11,7 +11,7 @@ mkpath("figs_ss")
 activFunc = tanh;
 nn = 100;
 
-expNum = 2;
+expNum = 4;
 fileLoc = "data_ss/exp$(expNum).jld2";
 
 mkpath("figs_ss/exp$(expNum)")
@@ -31,14 +31,15 @@ nIters = length(PDE_losses)
 semilogy(1:nIters, PDE_losses, label= "PDE_losses");
 semilogy(1:nIters, BC_losses, label = "BC_losses");
 legend();
-title("Loss Functions")
+title("Loss Functions Exp $(expNum)")
 tight_layout();
 savefig("figs_ss/exp$(expNum)/loss.png")
 
 
 ## Neural network
 dim = 4;
-chain = Chain(Dense(dim, nn, activFunc), Dense(nn, nn, activFunc), Dense(nn, 1));
+chain = Chain(Dense(dim, nn, activFunc), Dense(nn, nn, activFunc), Dense(nn, nn, activFunc), Dense(nn, 1)); # 3 hls
+# chain = Chain(Dense(dim, nn, activFunc), Dense(nn, nn, activFunc), Dense(nn, 1)); # 2 hls
 initθ,re  = Flux.destructure(chain)
 phi = (x,θ) -> re(θ)(Array(x))
 ## Domains
@@ -47,7 +48,7 @@ x2_min = deg2rad(-10f0); x2_max = deg2rad(10f0);
 x3_min = x2_min ; x3_max = x2_max ;
 x4_min = deg2rad(-5f0); x4_max = deg2rad(5f0);
 
-dx = [10f0; deg2rad(1f0); deg2rad(1f0); deg2rad(1f0);]; # discretization size used for training
+dx = [2.5f0; deg2rad(1f0); deg2rad(1f0); deg2rad(1f0);]; # discretization size used for training
 
 ## Generate functions to check solution and PDE error
 function ρ_pdeErr_fns(optParam)
@@ -67,10 +68,10 @@ end
 
 ## Grid discretization (not necessary if using Quadrature)
 # nEvalFine = 10; # to be changed
-x1Fine = collect(x1_min:dx[1]:x1_max);#range(xV_min, xV_max, length = nEvalFine);
-x2Fine = collect(x2_min:dx[2]:x2_max); #range(xα_min, xα_max, length = nEvalFine);
-x3Fine = collect(x3_min:dx[3]:x3_max);# range(xθ_min, xθ_max, length = nEvalFine);
-x4Fine = collect(x4_min:dx[4]:x4_max); #range(xq_min, xq_max, length = nEvalFine);
+x1Fine = collect(x1_min:dx[1]:x1_max);#range(x1_min, x1_max, length = nEvalFine);
+x2Fine = collect(x2_min:dx[2]:x2_max); #range(x2_min, x2_max, length = nEvalFine);
+x3Fine = collect(x3_min:dx[3]:x3_max);# range(x3_min, x3_max, length = nEvalFine);
+x4Fine = collect(x4_min:dx[4]:x4_max); #range(x4_min, x4_max, length = nEvalFine);
 
 # RHOFine = [ρFn([x1d, x2d, x3d, x4d]) for x1d in x1Fine, x2d in x2Fine, x3d in x3Fine, x4d in x4Fine]; # no need to compute true ρ for full state
 
@@ -91,41 +92,41 @@ function ρ12(x1,x2)
 end
 RHO12Fine = [ρ12(x1d, x2d) for x1d in x1Fine, x2d in x2Fine];
 RHO12Fine = RHO12Fine/trapz((x1Fine, x2Fine), RHO12Fine); # normalizing
-println("Marginal pdf for x1 and x2 computed.")
+println("Marginal pdf for V and α computed.")
 
 ## Marginal pdf for the states(V, q)
 function ρ14(xV,xq)
     # p = [xV, xα]; y = [xθ, xq]
     ρFnQuad23(y,p) = ρFn([xV;y;xq])
-    prob = QuadratureProblem(ρFnQuad23, [xα_min; xθ_min], [xα_max; xθ_max], p = 0)
+    prob = QuadratureProblem(ρFnQuad23, [x2_min; x3_min], [x2_max; x3_max], p = 0)
     sol = solve(prob,HCubatureJL(),reltol=1e-3,abstol=1e-3)
     return sol.u
 end
-# RHO14Fine = [ρ14(xvg, xqg) for xvg in xVFine, xqg in xqFine];
-# RHO14Fine = RHO14Fine/sol.u; # normalizing
-# println("Marginal pdf for V and q computed.")
+RHO14Fine = [ρ14(xvg, xqg) for xvg in x1Fine, xqg in x4Fine];
+RHO14Fine = RHO14Fine/sol.u; # normalizing
+println("Marginal pdf for V and q computed.")
 
 ## Marginal pdf for the states(α, q)
 function ρ24(xα,xq)
     ρFnQuad13(y,p) = ρFn([y[1];xα;y[2];xq])
-    prob = QuadratureProblem(ρFnQuad13, [xV_min; xθ_min], [xV_max; xθ_max], p = 0)
+    prob = QuadratureProblem(ρFnQuad13, [x1_min; x3_min], [x1_max; x3_max], p = 0)
     sol = solve(prob,HCubatureJL(),reltol=1e-3,abstol=1e-3)
     return sol.u
 end
-# RHO24Fine = [ρ24(xαg, xqg) for xαg in xαFine, xqg in xqFine];
-# RHO24Fine = RHO24Fine/sol.u; # normalizing
-# println("Marginal pdf for α and q computed.")
+RHO24Fine = [ρ24(xαg, xqg) for xαg in x2Fine, xqg in x4Fine];
+RHO24Fine = RHO24Fine/sol.u; # normalizing
+println("Marginal pdf for α and q computed.")
 
 ## Marginal pdf for the states(α, θ)
 function ρ23(xα, xθ)
     ρFnQuad14(y,p) = ρFn([y[1];xα;xθ;y[2]])
-    prob = QuadratureProblem(ρFnQuad14, [xV_min; xq_min], [xV_max; xq_max], p = 0)
+    prob = QuadratureProblem(ρFnQuad14, [x1_min; x4_min], [x1_max; x4_max], p = 0)
     sol = solve(prob,HCubatureJL(),reltol=1e-3,abstol=1e-3)
     return sol.u
 end
-# RHO23Fine = [ρ23(xαg, xθg) for xαg in xαFine, xθg in xθFine];
-# RHO23Fine = RHO23Fine/sol.u; # normalizing
-# println("Marginal pdf for α and θ computed.")
+RHO23Fine = [ρ23(xαg, xθg) for xαg in x2Fine, xθg in x3Fine];
+RHO23Fine = RHO23Fine/sol.u; # normalizing
+println("Marginal pdf for α and θ computed.")
 
 
 
@@ -152,11 +153,14 @@ function plotDistErr(figNum,x1Fine,x2Fine,RHOFine, label1, label2)
     tight_layout()
 
 end
-plotDistErr(12, x1Fine,x2Fine, RHO12Fine, "x1", "x2");
-# savefig("figs_ss/exp$(expNum).png");
-# plotDistErr(14, xVFine,xqFine, RHO14Fine, "V (ft/s)", "q (rad/s)");
-# plotDistErr(24, xαFine,xqFine, RHO24Fine, "α (rad)", "q (rad/s)");
-# plotDistErr(23, xαFine,xθFine, RHO23Fine, "α (rad)", "θ (rad");
+plotDistErr(12, x1Fine,x2Fine, RHO12Fine, "V (ft/s)", "α (rad)");
+savefig("figs_ss/exp$(expNum)/x12.png");
+plotDistErr(14, x1Fine,x4Fine, RHO14Fine, "V (ft/s)", "q (rad/s)");
+savefig("figs_ss/exp$(expNum)/x14.png");
+plotDistErr(24, x2Fine,x4Fine, RHO24Fine, "α (rad)", "q (rad/s)");
+savefig("figs_ss/exp$(expNum)/x24.png");
+plotDistErr(23, x2Fine,x3Fine, RHO23Fine, "α (rad)", "θ (rad)");
+savefig("figs_ss/exp$(expNum)/x23.png");
 
 # any(isnan.(RHO24Fine))
 ##
