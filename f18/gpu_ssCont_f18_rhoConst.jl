2d5db02357785baa1eaee8a25463c9129717745a
Vaishnav <vish0908@gmail.com>
2022-06-24 19:07:47 -0500

    exp8: sig=0.1.

--- f18/gpu_ssCont_f18_rhoConst.jl
+++ f18/gpu_ssCont_f18_rhoConst.jl
@@ -23,20 +23,20 @@ maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 # μ_ss = [0f0,0f0,0f0,0f0] #.+ Array(f18_xTrim[indX]);
 # Σ_ss = 0.01f0*Array(f18_xTrim[indX]).*1.0f0I(4);
 μ_ss = An*([0f0,0f0,0f0,0f0] .+ Array(f18_xTrim[indX])) + bn;
-Σ_ss = 0.01f0.*1.0f0I(4);
+Σ_ss = 0.1f0.*1.0f0I(4);
 # indU = [3]; # only using δ_stab for control
 
 Q_fpke = 0.0f0; # Q = σ^2
 
 # file location to save data
-expNum = 7;
+expNum = 8;
 useGPU = false;
 runExp = true;
 saveFile = "data_rhoConst_gpu/exp$(expNum).jld2";
 runExp_fileName = "out_rhoConst_gpu/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Generating a controller for f18 with desired ss distribution. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Not dividing equation by ρ. Finding utrim, using xN as input. Changed several things. useGPU = $(useGPU).
+        write(io, "Generating a controller for f18 with desired ss distribution. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Not dividing equation by ρ. Finding utrim, using xN as input. Changed several things. useGPU = $(useGPU). Changed Σ_ss.
         Experiment number: $(expNum)\n")
     end
 end
@@ -58,6 +58,7 @@ for i in 1:length(indX)
 end
 
 # F18 Dynamics
+maskTrim = ones(Float32,length(f18_xTrim)); maskTrim[indX] .= 0f0;
 function f(xn)
 
     # # ud = Kc1(xd[1],xd[2],xd[3],xd[4]); 
