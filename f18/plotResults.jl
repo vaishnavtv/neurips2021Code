baceda15c09dbeeed076ea063b6b81f73afded22
vaishnavtv <vish0908@gmail.com>
2022-06-25 17:59:22 -0500

    plot upd.

--- f18/plotResults.jl
+++ f18/plotResults.jl
@@ -2,6 +2,7 @@ using JLD2,PyPlot,LinearAlgebra,Trapz,Printf,LaTeXStrings, Flux, Distributions
 pygui(true);
 cd(@__DIR__);
 include("../utils.jl")
+include("f18DynNorm.jl")
 include("f18Dyn.jl")
 mkpath("figs_rhoFixed_gpu")
 
@@ -12,12 +13,15 @@ activFunc = tanh;
 suff = string(activFunc);
 nn = 100;
 Q_fpke = 0.0f0#*1.0I(2); # σ^2
-tEnd = 100.0; dt = 2.0;
+tEnd = 500.0f0; dt = tEnd/50f0;
 
-expNum = 6; 
+TMax = 50000f0; # maximum thrust
+dStab_max = pi/3; # min, max values for δ_stab
+
+expNum = 12; 
 # fileLoc = "data_rhoConst/exp$(expNum).jld2";
 fileLoc = "data_rhoConst_gpu/exp$(expNum).jld2";
-@info "Loading file from ss_cont_f18_rhoFixed exp $(expNum)"
+@info "Loading file from ss2_cont_f18_rhoFixed exp $(expNum)"
 file = jldopen(fileLoc, "r");
 optParam = Array(read(file, "optParam"));
 PDE_losses = read(file, "PDE_losses");
@@ -43,16 +47,18 @@ chain = Chain(Dense(dim, nn, activFunc), Dense(nn, nn, activFunc), Dense(nn, 1))
 initθ,re  = Flux.destructure(chain)
 phi = (x,θ) -> re(θ)(Array(x))
 
-th1 = optParam#[1:Int(length(optParam)/2)]; 
-# th2 = optParam[Int(length(optParam)/2) + 1:end];
+th1 = optParam[1:Int(length(optParam)/2)]; 
+th2 = optParam[Int(length(optParam)/2) + 1:end];
 
 ## F18 Dynamics with controller Kc
 using DifferentialEquations
 f18_xTrim = Float32.(1.0e+02*[3.500000000000000;-0.000000000000000;0.003540971009312;-0.000189987747098;0.000322083113778;0.000459982356948;0.006108652381980;0.003262466855376;0;]);
+# f18_xTrim = Float32.(1.0e+02*[5.122297754868714;-0.002063950074059;-0.000109726717731;0.000000000000000;-0.000000000000000;0.000000000000000;0.431781569741566;0.019209147318012;-0.000000008202862]);
 indX = [1;3;8;5];
 
 f18_uTrim = Float32.(1.0e+04 *[-0.000000767698465;-0.000002371697733;-0.000007859275313;1.449999997030301;]);
-indU = [3]#;4];
+# f18_uTrim = Float32.(1.0e+04*[-0.000005210083288;-0.000024039853679;-0.000010246234116;3.619055158682376]);
+indU = [3;4];
 
 maskIndx = zeros(Float32,(length(f18_xTrim),length(indX)));
 maskIndu = zeros(Float32,(length(f18_uTrim),length(indU)));
@@ -64,22 +70,26 @@ for i in 1:length(indX)
 end
 maskTrim = ones(Float32,length(f18_xTrim)); maskTrim[indX] .= 0f0; # keeping nonrelevant states constant
     
-function f18RedDyn(xd,t)
+# function f18RedDyn(xd,t)
+function f18RedDyn(xn,t)
     # reduced order f18 dynamics
-    ud = first(phi(xd, th1))
+    # ud = first(phi(xd, th1))
     # ud = [first(phi(xd, th1)); first(phi(xd, th2))]
     
-    tx = maskIndx*xd; tu = maskIndu*ud;
+    xi = An\(xn-bn);
+    ud = [dStab_max*tanh(first(phi(xn, th1))); TMax*sigmoid(first(phi(xn, th2)))]
     
-    xFull = f18_xTrim + maskIndx*xd; 
-    uFull = [1f0;1f0;0f0;1f0].*f18_uTrim + maskIndu*ud;
-    # uFull = f18_uTrim + maskIndu*ud;
+    # tx = maskIndx*xd; tu = maskIndu*ud;
+    
+    # xFull = f18_xTrim + maskIndx*xd; 
+    # uFull = [1f0;1f0;0f0;1f0].*f18_uTrim + maskIndu*ud;
+    # uFull = f18_uTrim #+ maskIndu*ud;
 
-    # xFull = maskTrim.*f18_xTrim + maskIndx*xd; 
-    # uFull = [1f0;1f0;0f0;0f0].*f18_uTrim + maskIndu*ud;
+    xFull = maskTrim.*f18_xTrim + maskIndx*xi; 
+    uFull = [1f0;1f0;0f0;0f0].*f18_uTrim + maskIndu*ud;
     
     xdotFull = f18Dyn(xFull, uFull) #- f18Dyn(f18_xTrim, f18_uTrim)
-    return (xdotFull[indX])
+    return An*(xdotFull[indX])
 end
 
 function nlSim(x0)
@@ -97,17 +107,20 @@ function angBounds(x)
 end
 
 #
-function plotTraj(sol, figNum)
-    tSim = sol.t
-    x1Sol = sol[1, :] #.- f18_xTrim[indX[1]]
-    x2Sol = (sol[2, :]) #.- f18_xTrim[indX[2]]
-    x3Sol = (sol[3, :]) #.- f18_xTrim[indX[3]]
-    x4Sol = sol[4, :] #.- f18_xTrim[indX[4]]
-    # u = [(1-exp(-τ*tSim[i]))*first(phi(sol.u[i], optParam)) for i = 1:length(tSim)]
-    u1 = [first(phi(sol.u[i], th1)) for i = 1:length(tSim)]# .- f18_uTrim[indU[1]];
+function plotTraj(solInp, figNum)
+    sol = hcat([An\(xt - bn) for xt in solInp.u]...)
+    tSim = solInp.t
+    x1Sol = sol[1, :] .- f18_xTrim[indX[1]]
+    x2Sol = (sol[2, :]) .- f18_xTrim[indX[2]]
+    x3Sol = (sol[3, :]) .- f18_xTrim[indX[3]]
+    x4Sol = sol[4, :] .- f18_xTrim[indX[4]]
+    # u1 = [first(phi(sol.u[i], th1)) for i = 1:length(tSim)]# .- f18_uTrim[indU[1]];
     # u2 = [first(phi(sol.u[i], th2)) for i = 1:length(tSim)]# .- f18_uTrim[indU[2]];
     # u2 = 0f0*ones(length(u1)) #*[first(phi(sol.u[i], th2)) for i = 1:length(tSim)];
 
+    u1 = [dStab_max*tanh(first(phi(solInp[:,i], th1))) for i = 1:length(tSim)]# .- f18_uTrim[indU[1]];
+    u2 = [TMax*sigmoid(first(phi(solInp[:,i], th2))) for i = 1:length(tSim)]# .- f18_uTrim[indU[2]];
+    
     figure(figNum, figsize = (8,4));#clf()
     subplot(3, 2, 1);
     PyPlot.plot(tSim, x1Sol);
@@ -129,26 +142,31 @@ function plotTraj(sol, figNum)
     PyPlot.plot(tSim, rad2deg.(u1))
     xlabel("t");  ylabel(L"δ_{stab} ({\rm deg})"); grid("on")
 
-    # subplot(3, 2, 6)
-    # PyPlot.plot(tSim, u2)
-    # xlabel("t"); ylabel("T"); grid("on")
+    subplot(3, 2, 6)
+    PyPlot.plot(tSim, u2)
+    xlabel("t"); ylabel("T"); grid("on")
 
     suptitle("Exp$(expNum) Traj: Deviation from x̄");
     tight_layout()
 end
 vmin = [-100f0;deg2rad(-10f0);deg2rad(-10f0); deg2rad(-5f0)] ;
-xmin = 1f0*vmin #.+ f18_xTrim[indX]; 
-xmax = -1f0*vmin #.+ f18_xTrim[indX]
-tx = xmin .+ ((xmax - xmin) .* rand(4));
+xmin = 1f0*vmin .+ f18_xTrim[indX]; 
+xmax = -1f0*vmin .+ f18_xTrim[indX]
+txFull = xmin .+ ((xmax - xmin) .* rand(4));
+tx = An*(txFull) + bn;
+@show txFull
 @show tx
 solSim = nlSim(tx);
+figure(3); clf();
+plotTraj(solSim, 3);
+# Unnormalize start and end values
+solSim[:,1] = An\(solSim[:,1] - bn); solSim[:,end] = An\(solSim[:,end] - bn);
 println("x1 initial value: $(solSim[1,1]);  x1 terminal value: $(solSim[1,end])");
 println("x2 initial value: $(rad2deg(solSim[2,1])) deg;  x2 terminal value: $(rad2deg(solSim[2,end])) deg");
 println("x3 initial value: $(rad2deg(solSim[3,1])) deg;  x3 terminal value: $(rad2deg(solSim[3,end])) deg");
 println("x4 initial value: $(rad2deg(solSim[4,1])) deg/s;  x4 terminal value: $(rad2deg(solSim[4,end])) deg/s");
 println("Terminal value state norm: $(norm(solSim[:,end]))");
-figure(3); clf();
-plotTraj(solSim, 3);
+
 savefig("figs_rhoFixed_gpu/exp$(expNum)/traj.png")
 # savefig("figs_rhoFixed/exp$(expNum)/traj.png")
 
