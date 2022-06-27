3fdc29f54386422a9078ae264e97500a36de7a19
Vaishnav <vish0908@gmail.com>
2022-06-27 12:23:44 -0500

    exp8: Q = 0, sig = 0.001

--- f18/cont_ss_f18.jl
+++ f18/cont_ss_f18.jl
@@ -19,13 +19,13 @@ maxOpt1Iters = 10000; # maximum number of training iterations for opt1
 opt2 = Optim.LBFGS(); # second optimizer used for fine-tuning
 maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 
-Q_fpke = 0.1f0; # Q = σ^2
+Q_fpke = 0.0f0; # Q = σ^2
 
 # parameters for rhoSS_desired
 # μ_ss = [0f0,0f0,0f0,0f0] #.+ Array(f18_xTrim[indX]);
 # Σ_ss = 0.01f0*Array(f18_xTrim[indX]).*1.0f0I(4);
 μ_ss = An2*([0f0,0f0,0f0,0f0] .+ Array(f18_xTrim[indX])) + bn2;
-Σ_ss = 0.01f0.*1.0f0I(4);
+Σ_ss = 0.001f0.*1.0f0I(4);
 
 TMax = 50000f0; # maximum thrust
 dStab_max = pi/3; # min, max values for δ_stab
@@ -34,14 +34,14 @@ dx = 0.1f0;
 indU = [3,4]; # only using δ_stab for control
 
 # file location to save data
-expNum = 7;
+expNum = 8;
 useGPU = true;
 runExp = true;
 saveFile = "data_ss1_cont/exp$(expNum).jld2";
 runExp_fileName = "out_ss1_cont/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Controller and ss distribution for the trimmed F18. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Using normalized variables between [-5,5], finding both δ_stab and T. Q_fpke = $(Q_fpke). dx = $(dx). Diffusion in α.
+        write(io, "Controller and ss distribution for the trimmed F18. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Using normalized variables between [-5,5], finding both δ_stab and T. Q_fpke = $(Q_fpke). dx = $(dx). Changed Σ_ss.
         Experiment number: $(expNum)\n")
     end
 end
