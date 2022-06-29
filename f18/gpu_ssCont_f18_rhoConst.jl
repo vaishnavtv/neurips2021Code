ab422ae7a8e924b3a1df9286d1de1b61fd54f31e
Vaishnav <vish0908@gmail.com>
2022-06-28 22:55:31 -0500

    exp28: pert dyn, sig = 0.0001

--- f18/gpu_ssCont_f18_rhoConst.jl
+++ f18/gpu_ssCont_f18_rhoConst.jl
@@ -24,7 +24,7 @@ maxOpt2Iters = 10000; # maximum number of training iterations for opt2
 # Σ_ss = 0.01f0*Array(f18_xTrim[indX]).*1.0f0I(4);
 # μ_ss = An2*([0f0,0f0,0f0,0f0] .+ Array(f18_xTrim[indX])) + bn2; # full dynamics
 μ_ss = An3*([0f0,0f0,0f0,0f0]) + bn3; # perturbation dynamics == 0
-Σ_ss = 0.001f0.*1.0f0I(4);
+Σ_ss = 0.0001f0.*1.0f0I(4);
 
 Q_fpke = 0.0f0; # Q = σ^2
 dx = 0.1f0;
@@ -39,7 +39,7 @@ saveFile = "data_rhoConst_gpu/exp$(expNum).jld2";
 runExp_fileName = "out_rhoConst_gpu/log$(expNum).txt";
 if runExp
     open(runExp_fileName, "a+") do io
-        write(io, "Generating a controller for f18 with desired ss distribution. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Not dividing equation by ρ. Finding utrim, using xN as input. dx = $(dx). Added dStab_max and TMax with tanh and sigmoid activation functions on output for δ_stab and Thrust. Changed normalized variable bounds to [-5,5] instead of [0,1]. Changed Σ_ss to $(Σ_ss). Check ADAM iters. Adding utrim. Perturbation dynamics instead of full dynamics. Using Kc_nomStab.
+        write(io, "Generating a controller for f18 with desired ss distribution. 2 HL with $(nn) neurons in the hl and $(activFunc) activation. $(maxOpt1Iters) iterations with ADAM and then $(maxOpt2Iters) with LBFGS. using GPU? $(useGPU). Q_fpke = $(Q_fpke). μ_ss = $(μ_ss). Σ_ss = $(Σ_ss). Not dividing equation by ρ. Finding utrim, using xN as input. dx = $(dx). Added dStab_max and TMax with tanh and sigmoid activation functions on output for δ_stab and Thrust. Changed normalized variable bounds to [-5,5] instead of [0,1]. Changed Σ_ss to $(Σ_ss). Check ADAM iters. Adding utrim. Perturbation dynamics instead of full dynamics. Lowering Σ_ss even further.
         Experiment number: $(expNum)\n")
     end
 end
@@ -59,7 +59,7 @@ for i in 1:length(indX)
         maskIndu[indU[i],i] = 1f0;
     end
 end
-
+maskK = Float32.(maskIndu*Kc_nomStab) # masking linear controller in perturbation
 # F18 Dynamics
 maskTrim = ones(Float32,length(f18_xTrim)); maskTrim[indX] .= 0f0;
 function f(xn)
@@ -100,11 +100,11 @@ function f(xn)
     # return An2*(xdotFull[indX]) # return the 4 state dynamics in normalized form
 
     # normalized input to f18 dynamics (xn is perturbation)
-    xi = An3Inv*(xn .- bn3); # x of 'i'nterest
+    xi = An3Inv*(xn .- bn3); # x of 'i'nterest,, perturbation
     ui = [dStab_max*Kc1((xn)...), TMax*Kc2((xn)...)];
 
     xFull = f18_xTrim + maskIndx*xi;
-    uFull = f18_uTrim + maskIndu*ui + maskIndu*Kc_nomStab*xi;
+    uFull = f18_uTrim + maskIndu*ui;
 
      xdotFull = f18Dyn(xFull, uFull)
     return An3*(xdotFull[indX]) # return the 4 state dynamics in normalized form
